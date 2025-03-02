a5892ffa5548ace9ae9b7327daf6f32d465c4f33
awelvor <philippe.regent@gmail.com>
2025-03-02 07:30:20 +0100

    Update iojv4204.sql

--- iojv4204.sql
+++ iojv4204.sql
@@ -19,7 +19,19 @@ select option_value from iojv4204_wp810.wpjp_options where option_name="blogname
 delete from iojv4204_wp810.wpjp_posts where post_type="revision";
 delete from iojv4204_wp810.wpjp_posts where post_status="auto-draft";
 delete from iojv4204_wp810.wpjp_posts where post_status="trash";
-select valeur from iojv4204_spip007.spip_meta where nom='descriptif_site';
+
+
+
+
+
+select option_value from  iojv4204_wp41.wpwf_options where option_name="blogname";
+select option_value from iojv4204_wp275.wpcn_options where option_name="blogname";
+select option_value from iojv4204_wp352.wphc_options where option_name="blogname";
+select option_value from iojv4204_wp810.wpjp_options where option_name="blogname";
+delete from iojv4204_wp41.wpwf_posts where post_type="revision";
+delete from iojv4204_wp275.wpcn_posts where post_type="revision";
+delete from iojv4204_wp352.wphc_posts where post_type="revision";
+delete from iojv4204_wp810.wpjp_posts where post_type="revision";
 
 
 
